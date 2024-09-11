9a6e84bf2e9b463e07551815c8629cbd8e20a41a
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-09-12 02:22:03 +0600

    full code

---
+++ src/ComingSoon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <div className="coming-soon-content">
+        <h1>Coming Soon</h1>
+        <p>We are working hard to bring something awesome!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
